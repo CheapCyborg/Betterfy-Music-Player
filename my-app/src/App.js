6368592756c225ca1950673a8fdc970bc2d334e1
Damian <damiang58@gmail.com>
2018-06-16 18:32:12 -0400

    Changed UI a bit as well as add a recently played button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,10 +13,20 @@ class App extends Component {
     this.state = {
       loggedIn: accessToken ? true : false,
       nowPlaying: {
-        name: 'Not Checked',
-        image: ''
+        name: ' Not Checked',
+        image: '',
+        artists: ' Not Checked'
+      },
+      recentlyPlayed: {
+        name1: '',
+        image: '',
+        artists: ' Not Checked'
       }
     }
+    this.state.poop =  {
+      loggedIn: accessToken ? true : false,
+
+  }
     if(params.access_token){
       spotifyWebApi.setAccessToken(accessToken)
     }
@@ -36,25 +46,51 @@ class App extends Component {
       this.setState({
         nowPlaying: {
           name: response.item.name,
-          image: response.item.album.images[0].url
+          image: response.item.album.images[0].url,
+          artists: response.item.artists[0].name
+        }
+      })
+    })
+  }
+  getRecentlyPlayed(){
+    spotifyWebApi.getMyRecentlyPlayedTracks()
+    .then((response) => {
+      for(var i=0; i < response.legnth; i++) {
+      }
+      this.setState({
+        recentlyPlayed: {
+          name: response.items[i].track.album.name
         }
       })
     })
   }
   render() {
     return (
-      <div class="container">
+      <div class="container-fluid">
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to MixCloud</h1>
             </header>
             <a href="http://localhost:8888/" class="btn btn-dark btn-lg active" role="button" aria-pressed="true">Login using Spotify</a>
-              <div>Now Playing: { this.state.nowPlaying.name }</div>
+              <div class='text-left'>
+                Now Playing: { this.state.nowPlaying.name }
+              </div>
+              <div class='text-left mx-auto'>
+              By: { this.state.nowPlaying.artists }
+              </div>
+              <div class='row'>
+                <button style={{marigin: 20}} class='btn col-2' onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+              </div>
               <div>
-                <img class="img-thumbnail" src={ this.state.nowPlaying.image } style={{width: 400}}/>
+                <img class="img float-left rounded" src={ this.state.nowPlaying.image } style={{width: 400}}/>
             </div>
-            <button onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+            <div class="row">
+              Recently Played: { this.state.recentlyPlayed.name }
+            </div>
+            <div class='row'>
+              <button class='btn' onClick={() => this.getRecentlyPlayed()}>Check Recently Played</button>
+            </div>
             </div>
           </div>
     );
