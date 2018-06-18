8f8977ea610707a4c7c9236adf6574d3bcef9024
“Damian <damiang58@gmail.com>
2018-06-18 13:17:31 -0400

    added album search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,25 +12,33 @@ class App extends Component {
     var accessToken = params.access_token;
     this.state = {
       loggedIn: accessToken ? true : false,
+      value: '',
       nowPlaying: {
         name: ' Not Checked',
         image: '',
         artists: ' Not Checked'
       },
       recentlyPlayed: {
-        name1: '',
+        name: '',
         image: '',
         artists: ' Not Checked'
+      },
+      searchAlbums: {
+        name: '',
+        artists: ''
       }
-    }
-    this.state.poop =  {
-      loggedIn: accessToken ? true : false,
-
-  }
+    };
+    this.handleChange = this.handleChange.bind(this);
+    
     if(params.access_token){
       spotifyWebApi.setAccessToken(accessToken)
     }
   }
+  handleChange(event) {
+      this.setState({
+        value: event.target.value
+      });
+    }
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
@@ -55,41 +63,56 @@ class App extends Component {
   getRecentlyPlayed(){
     spotifyWebApi.getMyRecentlyPlayedTracks()
     .then((response) => {
-      for(var i=0; i < response.legnth; i++) {
-      }
       this.setState({
         recentlyPlayed: {
-          name: response.items[i].track.album.name
+          name: response.items[0].track.album.name,
+          image: response.items[0].track.album.images[0].url,
+          artists: response.items[0].track.artists[0].name
         }
       })
     })
   }
+  searchAlbums(){
+    spotifyWebApi.searchAlbums(this.state.value)
+
+  }
+
   render() {
     return (
       <div class="container-fluid">
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to MixCloud</h1>
+            <h1 className="App-title">Welcome to Betterfy</h1>
             </header>
+            <form class="form-inline">
+              <input type="text" id="searchAlbums" class='' value={this.state.value} onChange={this.handleChange} placeholder="Search for an Album"/>
+              <button class="btn btn-small" id="clickMe" type="button" onClick={() => this.searchAlbums()}>Search</button>
+            </form>
             <a href="http://localhost:8888/" class="btn btn-dark btn-lg active" role="button" aria-pressed="true">Login using Spotify</a>
-              <div class='text-left'>
+              <div>
                 Now Playing: { this.state.nowPlaying.name }
               </div>
-              <div class='text-left mx-auto'>
+              <div>
               By: { this.state.nowPlaying.artists }
               </div>
-              <div class='row'>
-                <button style={{marigin: 20}} class='btn col-2' onClick={() => this.getNowPlaying()}>Check Now Playing</button>
+              <div>
+                <button style={{marigin: 20}} class='btn' onClick={() => this.getNowPlaying()}>Check Now Playing</button>
               </div>
               <div>
-                <img class="img float-left rounded" src={ this.state.nowPlaying.image } style={{width: 400}}/>
+                <img class="img rounded" src={ this.state.nowPlaying.image } style={{width: 400}}/>
             </div>
-            <div class="row">
+            <div>
               Recently Played: { this.state.recentlyPlayed.name }
             </div>
-            <div class='row'>
+            <div>
+            By: { this.state.recentlyPlayed.artists }
+            </div>
+            <div>
               <button class='btn' onClick={() => this.getRecentlyPlayed()}>Check Recently Played</button>
+            </div>
+            <div>
+              <img class="img rounded" src={ this.state.recentlyPlayed.image } style={{width: 400}}/>
             </div>
             </div>
           </div>
