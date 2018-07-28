a5dade4f13af6fbfb8ec9066dfa3aa1747d433fd
Damian <damiang58@gmail.com>
2018-07-28 01:00:49 -0400

    u7

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import {Comment, User} from "./models/comment.js";
 const app = express();
 const router = express.Router();
 
-const API_PORT = process.env.API_PORT;
+const API_PORT = process.env.PORT;
 
 app.use(
     bodyParser.urlencoded({
